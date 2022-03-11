4a25a82c9ffb52563d70e5d456ea8df80580ad68
Josh <jcjohnson77@gmail.com>
2022-03-11 08:52:20 -0500

    Add markdown formatting for results

--- scripts/test.js
+++ scripts/test.js
@@ -49,10 +49,10 @@ const execute = (cmd, showCommand = true) => {
 }
 
 const tests = {
-  children: [1, 3, 5, 8],
-  depth: [1, 3, 5],
+  children: [1, 3],
+  depth: [1, 3],
   styles: [true, false],
-  iterations: 5,
+  iterations: 1,
 }
 
 async function test(bundler, children, depth, styles, iteration) {
@@ -79,25 +79,69 @@ async function test(bundler, children, depth, styles, iteration) {
   }
 
   // TODO average, better output, get size of bundle, etc?
-  console.log(`${bundler} ${status} after ${elapsed}s with ${children} children and a depth of ${depth}. Bundle size: ${bundleSize}`)
+  console.log(`${bundler} ${status} after ${elapsed}s with c=${children}, d=${depth}, and styles=${styles ? 'true' : 'false'}. Bundle size: ${bundleSize}`)
+
+  return {
+    success,
+    time: elapsed, 
+    size: bundleSize
+  }
+}
+
+function formatResults(c, d, s, results) {
+  const bundlers = Object.keys(results).sort()
+  const success = bundlers.map((b) => results[b].success * 100 + "%")
+  const time = bundlers.map((b) => results[b].time + "s")
+  const size = bundlers.map((b) => Math.round(results[b].size / 1024) + "kb")
+
+  const output = [
+    `### Results with children=${c}, depth=${d}, and styles=${s ? 'true' : 'false'}`,
+    `||${bundlers.join("|")}|`,
+    `|---|${bundlers.map(() => "---").join("|")}|`,
+    `|Success Rate|${success.join("|")}|`,
+    `|Build Time|${time.join("|")}|`,
+    `|Bundle Size|${size.join("|")}|`,
+  ]
+
+  return output.join("\n")
 }
 
 // TODO run two builds to measure second build
 async function runTests() {
+  let finalResults = []
   for (const c of tests.children) {
     for (const d of tests.depth) {
       for (const s of tests.styles) {
         await execute(`yarn generate -c ${c} -d ${d} -s ${s ? 'true' : 'false'}`)
 
+        let results = {}
+
         for (bundler of bundlers) {
+          let final = {success: 0, time: 0, size: 0}
+          
           for (const i of [...Array(tests.iterations).keys()]) {
-            await test(bundler, c, d, s, i)
+            const result = await test(bundler, c, d, s, i)
+            final.success += result.success ? 1 : 0
+            if (result.success) {
+              final.time += result.time
+              final.size += result.size
+            }
+          }
+
+          results[bundler] = {
+            success: final.success / tests.iterations,
+            time: final.time / tests.iterations,
+            size: final.size / tests.iterations,
           }
         }
-        
+
+        const formatted = formatResults(c, d, s, results)
+        finalResults.push(formatted)
       }
     }
   }
+
+  fs.writeFileSync(path.resolve(__dirname, '..', 'results.md'), finalResults.join("\n\n"))
 }
 
 runTests()
