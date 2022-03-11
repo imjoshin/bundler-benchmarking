142bb4299d4d586754c9169a27170e0b48c587d0
Josh <jcjohnson77@gmail.com>
2022-03-10 23:13:12 -0500

    rollup isn't quite right, but progress

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,29 @@
-import typescript from '@rollup/plugin-typescript';
-import css from "rollup-plugin-import-css";
-import pkg from './package.json';
+import typescript from '@rollup/plugin-typescript'
+import css from "rollup-plugin-import-css"
+import babel from 'rollup-plugin-babel';
+import injectProcessEnv from 'rollup-plugin-inject-process-env';
+import pkg from './package.json'
 
 export default {
   input: './src/index.tsx',
   output: {
     file: pkg.main,
-    format: 'es',
+    format: 'iife',
     sourcemap: false,
+    globals: {
+      'react': 'React',
+      'react-dom': 'ReactDOM',
+    }
   },
   plugins: [
     typescript(),
     css(),
+    injectProcessEnv({ 
+      BUNDLER_NAME: 'rollup',
+    }),
+    babel({ 
+        exclude: 'node_modules/**',
+        presets: ['@babel/env', '@babel/preset-react']
+    })
   ]
 }
